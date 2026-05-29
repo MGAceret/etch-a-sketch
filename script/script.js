11f333a63f3058d3c59ebc55479d974d2d0994fc
Xyans Yes <mgaceret@gmail.com>
2026-05-29 18:52:45 +0800

    Created placed another loop to successfully create a 16x16 grid with divs

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
 function test() {
 for (let i=0; i < 16; i++) {
-    let display = document.querySelector(".gridDisplay");
-    let gridDiv = document.createElement("div");
-    display.appendChild(gridDiv)
-    console.log("A div has been created")
+    for (let i=0; i < 16; i++) {
+        let display = document.querySelector(".gridDisplay");
+        let gridDiv = document.createElement("div");
+        display.appendChild(gridDiv)
+        gridDiv.innerText = "lorem"
+        console.log("A div has been created")
+    }
 }
 }
