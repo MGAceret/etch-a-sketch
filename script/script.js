24b4f374e38f6eb549ff58b80e3f30415c3afe58
Xyans Yes <mgaceret@gmail.com>
2026-05-30 20:10:41 +0800

    Changed function name to gridCreation and added indentation to the function code

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
-function test() {
-for (let i=0; i < 16; i++) {
+function gridCreation() {
     for (let i=0; i < 16; i++) {
-        let display = document.querySelector(".gridDisplay");
-        let gridDiv = document.createElement("div");
-        display.appendChild(gridDiv)
-        gridDiv.innerText = "lorem"
-        console.log("A div has been created")
+        for (let i=0; i < 16; i++) {
+            let display = document.querySelector(".gridDisplay");
+            let gridDiv = document.createElement("div");
+            display.appendChild(gridDiv)
+            gridDiv.innerText = "lorem"
+            console.log("A div has been created")
+        }
     }
-}
 }
